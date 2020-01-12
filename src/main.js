d8d2d7aad34367dd7a83d232f8eeb0884dfcac1d
chenxf123 <1632728273@qq.com>
2020-01-12 12:47:51 +0800

    注释掉了一些东西

--- src/main.js
+++ src/main.js
@@ -5,18 +5,18 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import echarts from 'echarts'
+// import echarts from 'echarts'
 // import VCharts from 'v-charts'
 
 
 
 // Vue.use(VCharts)
-Vue.prototype.$echarts = echarts
+// Vue.prototype.$echarts = echarts
 Vue.config.productionTip = false
 Vue.use(ElementUI);
 
 
-/* eslint-disable no-new */
+/* eslint-disable no-new */ 
 new Vue({
   el: '#app',
   router,
