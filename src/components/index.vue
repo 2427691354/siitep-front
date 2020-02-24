c3c2b22327fd0c630b56ac782ac303639651a950
chenxf123 <1632728273@qq.com>
2020-02-24 22:46:39 +0800

    。

--- src/components/index.vue
+++ src/components/index.vue
@@ -76,7 +76,7 @@
             <div class="alltitle">防疫宣传视频</div>
             <div class="allnav" id="echart4">
               <video-player
-                class="video-player vjs-custom-skin"
+                class="video-player vjs-custom-skin myvideo"
                 ref="videoPlayer"
                 :options="playerOptions2"
               ></video-player>
@@ -134,7 +134,7 @@ export default {
           {
             // 流配置，数组形式，会根据兼容顺序自动切换
             type: "rtmp/hls",
-            src: "rtmp://202.69.69.180:443/webcast/bshdlive-pc"
+            src: "rtmp://58.200.131.2:1935/livetv/hunantv"
           }
         ],
         poster: "", //你的封面地址
@@ -151,7 +151,7 @@ export default {
         playbackRates: [0.7, 1.0, 1.5, 2.0], //播放速度
         autoplay: true, //如果true,浏览器准备好时开始回放。
         muted: false, // 默认情况下将会消除任何音频。
-        loop: false, // 导致视频一结束就重新开始。
+        loop: true, // 导致视频一结束就重新开始。
         preload: "auto", // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
         language: "zh-CN",
         // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
