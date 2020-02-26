5dec050241db7fde522fe0a77961afda82baa55e
zffice <1318694378@qq.com>
2020-02-26 16:01:04 +0800

    地图

--- src/components/index.vue
+++ src/components/index.vue
@@ -50,18 +50,14 @@
                 src="../assets/picture/未带口罩.png"
                 alt=""
                 style="width:calc(100% / 6 - 3px)"
-              /> -->
+              />-->
             </div>
             <div class="boxfoot"></div>
           </div>
           <div class="boxall" style="height: 4.7rem;">
             <div class="alltitle">模块标题样式</div>
             <div class="allnav" id="echart3">
-              <el-tabs
-                v-model="activeName"
-                @tab-click="handleClick"
-                class="tab"
-              >
+              <el-tabs v-model="activeName" @tab-click="handleClick" class="tab">
                 <el-tab-pane label="热搜" name="first">热搜</el-tab-pane>
                 <el-tab-pane label="知识" name="second">知识</el-tab-pane>
                 <el-tab-pane label="辟谣" name="third">辟谣</el-tab-pane>
@@ -93,7 +89,7 @@
           <div class="boxallcard" style="height: 2.6rem">
             <!-- <div class="alltitle">停课不停学图片走马灯卡片式轮播</div> -->
             <div class="allnav" id="echart5">
-              <el-carousel :interval="2000" type="card" height="160px">
+              <el-carousel :interval="2000" type="card" height="2rem">
                 <el-carousel-item v-for="item in this.img_list" :key="item.img">
                   <img :src="'/static/img/' + item.img" />
                 </el-carousel-item>
@@ -114,10 +110,25 @@
             </div>
             <div class="boxfoot"></div>
           </div>
-          <div class="boxall" style="height: 3.2rem">
-            <div class="alltitle">模块标题样式</div>
-            <div class="allnav" id="echart5"></div>
-            <div class="boxfoot"></div>
+          <div class="boxallinfo" style="height: 3.2rem">
+            <!-- <div class="alltitle">模块标题样式</div> -->
+            <div class="allnav" id="echart5">
+              <el-table
+                :data="tableData"
+                border
+                style="width: 100%;font-size: 10px"
+                max-height="250"
+                :row-style="{height:'5px'}"
+                :cell-style="{padding:'0px'}"
+              >
+                >
+                <el-table-column prop="name" label="姓名" width="80%"></el-table-column>
+                <el-table-column prop="class" label="班级" width="80%"></el-table-column>
+                <el-table-column prop="tem" label="体温" width="80%"></el-table-column>
+                <el-table-column prop="status" label="状态"></el-table-column>
+              </el-table>
+            </div>
+            <div class="boxfootinfo"></div>
           </div>
           <div class="boxall" style="height: 2.7rem">
             <div class="alltitle">模块标题样式</div>
@@ -146,7 +157,9 @@ export default {
         sumIsolated: 0,
         sumHever: 0
       },
+      //轮播图图片
       img_list: [],
+
       // 图片父容器高度
       bannerHeight: 1000,
       // 浏览器宽度
@@ -216,7 +229,61 @@ export default {
           remainingTimeDisplay: false,
           fullscreenToggle: true //全屏按钮
         }
-      }
+      },
+      tableData: [
+        {
+          name: "王小虎",
+          class: "软件18C1",
+          tem: "36.7",
+          status: "正常"
+        },
+        {
+          name: "王小虎",
+          class: "软件18C1",
+          tem: "36.7",
+          status: "正常"
+        },
+        {
+          name: "王小虎",
+          class: "软件18C1",
+          tem: "36.7",
+          status: "正常"
+        },
+        {
+          name: "王小虎",
+          class: "软件18C1",
+          tem: "36.7",
+          status: "正常"
+        },
+        {
+          name: "王小虎",
+          class: "软件18C1",
+          tem: "36.7",
+          status: "正常"
+        },
+        {
+          name: "王小虎",
+          class: "软件18C1",
+          tem: "36.7",
+          status: "正常"
+        },
+        {
+          name: "王小虎",
+          class: "软件18C1",
+          tem: "36.7",
+          status: "正常"
+        },
+        {
+          name: "王小虎",
+          class: "软件18C1",
+          tem: "36.7",
+          status: "正常"
+        }
+      ],
+      //
+      data_alllist: [],
+      data_gelilist: [],
+      data_fashaolist: []
     };
   },
   components: {
@@ -229,6 +296,14 @@ export default {
 
     // 宏观统计 总人数、隔离人数、发烧人数
     this.initSum();
+    //学生各省物理分布人数
+    this.allnum();
+
+    //学生隔离人数
+    this.geli();
+
+    //学生发烧人数
+    this.fashao();
 
     //轮播图
     this.slideShow();
@@ -245,6 +320,9 @@ export default {
   methods: {
     handleClick(tab, event) {
       console.log(tab, event);
+    },
+    handleClick2(row) {
+      alert(row);
     },
     setSize: function() {
       // 通过浏览器宽度(图片宽度)计算高度
@@ -279,6 +357,48 @@ export default {
           // window.location.reload();
         });
     },
+    allnum() {
+      var self = this;
+      self.$http
+        .get(this.baseUrl + "/dayrpt/getStuInProvince")
+        .then(function(response) {
+          var res = response.data;
+          // console.log(res);
+          for (var i = 0; i < res.length; i++) {
+            self.data_alllist = self.data_alllist.concat({
+              "name": res[i].location_province,
+              "value": res[i].count
+            })
+          }
+        });
+        alert(self.data_alllist.length)
+    },
+    geli() {
+      var self = this;
+      self.$http
+        .get(this.baseUrl + "/dayrpt/getStuIsolatedInProvince")
+        .then(function(response) {
+          var res = response.data;
+          // console.log(res);
+          self.data_gelilist = res;
+        });
+    },
+    fashao() {
+      var self = this;
+      self.$http
+        .get(this.baseUrl + "/dayrpt/getStuHotInProvince")
+        .then(function(response) {
+          var res = response.data;
+          // self.data_fashaolist = res;
+          // console.log(res);
+          for (var i = 0; i < res.length; i++) {
+            self.data_fashaolist.push({
+              name: res[i].location_province,
+              value: res[i].count
+            });
+          }
+        });
+    },
     resizeFontsize() {
       var width = document.documentElement.clientWidth;
       document.documentElement.style.fontSize = width / 20 + "px";
@@ -287,198 +407,8 @@ export default {
     map() {
       // 基于准备好的dom，初始化echarts实例
       var myChart = this.$echarts.init(document.getElementById("map_1"));
-      var data = [
-        { name: "海门", value: 69 },
-        { name: "鄂尔多斯", value: 12 },
-        { name: "招远", value: 12 },
-        { name: "舟山", value: 12 },
-        { name: "齐齐哈尔", value: 14 },
-        { name: "盐城", value: 15 },
-        { name: "赤峰", value: 16 },
-        { name: "青岛", value: 18 },
-        { name: "乳山", value: 18 },
-        { name: "金昌", value: 19 },
-        { name: "泉州", value: 21 },
-        { name: "莱西", value: 21 },
-        { name: "日照", value: 21 },
-        { name: "胶南", value: 22 },
-        { name: "南通", value: 23 },
-        { name: "拉萨", value: 24 },
-        { name: "云浮", value: 24 },
-        { name: "梅州", value: 25 },
-        { name: "文登", value: 25 },
-        { name: "上海", value: 25 },
-        { name: "攀枝花", value: 25 },
-        { name: "威海", value: 25 },
-        { name: "承德", value: 25 },
-        { name: "厦门", value: 26 },
-        { name: "汕尾", value: 26 },
-        { name: "潮州", value: 26 },
-        { name: "丹东", value: 27 },
-        { name: "太仓", value: 27 },
-        { name: "曲靖", value: 27 },
-        { name: "烟台", value: 28 },
-        { name: "福州", value: 29 },
-        { name: "瓦房店", value: 30 },
-        { name: "即墨", value: 30 },
-        { name: "抚顺", value: 31 },
-        { name: "玉溪", value: 31 },
-        { name: "张家口", value: 31 },
-        { name: "阳泉", value: 31 },
-        { name: "莱州", value: 32 },
-        { name: "湖州", value: 32 },
-        { name: "汕头", value: 32 },
-        { name: "昆山", value: 33 },
-        { name: "宁波", value: 33 },
-        { name: "湛江", value: 33 },
-        { name: "揭阳", value: 34 },
-        { name: "荣成", value: 34 },
-        { name: "连云港", value: 35 },
-        { name: "葫芦岛", value: 35 },
-        { name: "常熟", value: 36 },
-        { name: "东莞", value: 36 },
-        { name: "河源", value: 36 },
-        { name: "淮安", value: 36 },
-        { name: "泰州", value: 36 },
-        { name: "南宁", value: 37 },
-        { name: "营口", value: 37 },
-        { name: "惠州", value: 37 },
-        { name: "江阴", value: 37 },
-        { name: "蓬莱", value: 37 },
-        { name: "韶关", value: 38 },
-        { name: "嘉峪关", value: 38 },
-        { name: "广州", value: 38 },
-        { name: "延安", value: 38 },
-        { name: "太原", value: 39 },
-        { name: "清远", value: 39 },
-        { name: "中山", value: 39 },
-        { name: "昆明", value: 39 },
-        { name: "寿光", value: 40 },
-        { name: "盘锦", value: 40 },
-        { name: "长治", value: 41 },
-        { name: "深圳", value: 41 },
-        { name: "珠海", value: 42 },
-        { name: "宿迁", value: 43 },
-        { name: "咸阳", value: 43 },
-        { name: "铜川", value: 44 },
-        { name: "平度", value: 44 },
-        { name: "佛山", value: 44 },
-        { name: "海口", value: 44 },
-        { name: "江门", value: 45 },
-        { name: "章丘", value: 45 },
-        { name: "肇庆", value: 46 },
-        { name: "大连", value: 47 },
-        { name: "临汾", value: 47 },
-        { name: "吴江", value: 47 },
-        { name: "石嘴山", value: 49 },
-        { name: "沈阳", value: 50 },
-        { name: "苏州", value: 50 },
-        { name: "茂名", value: 50 },
-        { name: "嘉兴", value: 51 },
-        { name: "长春", value: 51 },
-        { name: "胶州", value: 52 },
-        { name: "银川", value: 52 },
-        { name: "张家港", value: 52 },
-        { name: "三门峡", value: 53 },
-        { name: "锦州", value: 54 },
-        { name: "南昌", value: 54 },
-        { name: "柳州", value: 54 },
-        { name: "三亚", value: 54 },
-        { name: "自贡", value: 56 },
-        { name: "吉林", value: 56 },
-        { name: "阳江", value: 57 },
-        { name: "泸州", value: 57 },
-        { name: "西宁", value: 57 },
-        { name: "宜宾", value: 58 },
-        { name: "呼和浩特", value: 58 },
-        { name: "成都", value: 58 },
-        { name: "大同", value: 58 },
-        { name: "镇江", value: 59 },
-        { name: "桂林", value: 59 },
-        { name: "张家界", value: 59 },
-        { name: "宜兴", value: 59 },
-        { name: "北海", value: 60 },
-        { name: "西安", value: 61 },
-        { name: "金坛", value: 62 },
-        { name: "东营", value: 62 },
-        { name: "牡丹江", value: 63 },
-        { name: "遵义", value: 63 },
-        { name: "绍兴", value: 63 },
-        { name: "扬州", value: 64 },
-        { name: "常州", value: 64 },
-        { name: "潍坊", value: 65 },
-        { name: "重庆", value: 66 },
-        { name: "台州", value: 67 },
-        { name: "南京", value: 67 },
-        { name: "滨州", value: 70 },
-        { name: "贵阳", value: 71 },
-        { name: "无锡", value: 71 },
-        { name: "本溪", value: 71 },
-        { name: "克拉玛依", value: 72 },
-        { name: "渭南", value: 72 },
-        { name: "马鞍山", value: 72 },
-        { name: "宝鸡", value: 72 },
-        { name: "焦作", value: 75 },
-        { name: "句容", value: 75 },
-        { name: "北京", value: 79 },
-        { name: "徐州", value: 79 },
-        { name: "衡水", value: 80 },
-        { name: "包头", value: 80 },
-        { name: "绵阳", value: 80 },
-        { name: "乌鲁木齐", value: 84 },
-        { name: "枣庄", value: 84 },
-        { name: "杭州", value: 84 },
-        { name: "淄博", value: 85 },
-        { name: "鞍山", value: 86 },
-        { name: "溧阳", value: 86 },
-        { name: "库尔勒", value: 86 },
-        { name: "安阳", value: 90 },
-        { name: "开封", value: 90 },
-        { name: "济南", value: 92 },
-        { name: "德阳", value: 93 },
-        { name: "温州", value: 95 },
-        { name: "九江", value: 96 },
-        { name: "邯郸", value: 98 },
-        { name: "临安", value: 99 },
-        { name: "兰州", value: 99 },
-        { name: "沧州", value: 100 },
-        { name: "临沂", value: 103 },
-        { name: "南充", value: 104 },
-        { name: "天津", value: 105 },
-        { name: "富阳", value: 106 },
-        { name: "泰安", value: 112 },
-        { name: "诸暨", value: 112 },
-        { name: "郑州", value: 313 },
-        { name: "哈尔滨", value: 114 },
-        { name: "聊城", value: 116 },
-        { name: "芜湖", value: 117 },
-        { name: "唐山", value: 119 },
-        { name: "平顶山", value: 119 },
-        { name: "邢台", value: 119 },
-        { name: "德州", value: 120 },
-        { name: "济宁", value: 120 },
-        { name: "荆州", value: 127 },
-        { name: "宜昌", value: 130 },
-        { name: "义乌", value: 132 },
-        { name: "丽水", value: 133 },
-        { name: "洛阳", value: 134 },
-        { name: "秦皇岛", value: 136 },
-        { name: "株洲", value: 143 },
-        { name: "石家庄", value: 147 },
-        { name: "莱芜", value: 148 },
-        { name: "常德", value: 152 },
-        { name: "保定", value: 153 },
-        { name: "湘潭", value: 154 },
-        { name: "金华", value: 157 },
-        { name: "岳阳", value: 169 },
-        { name: "长沙", value: 175 },
-        { name: "衢州", value: 177 },
-        { name: "廊坊", value: 193 },
-        { name: "菏泽", value: 194 },
-        { name: "合肥", value: 229 },
-        { name: "武汉", value: 273 },
-        { name: "大庆", value: 279 }
-      ];
+
+      //地理位置
       var geoCoordMap = {
         海门: [121.15, 31.89],
         鄂尔多斯: [109.781327, 39.608266],
@@ -682,6 +612,7 @@ export default {
             });
           }
         }
+
         return res;
       };
 
@@ -697,13 +628,13 @@ export default {
         },
         tooltip: {
           trigger: "item",
-          formatter: function(params) {
-            if (typeof params.value[2] == "undefined") {
-              return params.name + " : " + params.value;
-            } else {
-              return params.name + " : " + params.value[2];
-            }
-          }
+          // formatter: function(params) {
+          //   if (typeof params.value[2] == "undefined") {
+          //     return params.name + " : " + params.value;
+          //   } else {
+          //     return params.name + " : " + params.value[2];
+          //   }
+          // }
         },
         legend: {
           data: ["隔离人数", "发烧人数"],
@@ -711,6 +642,20 @@ export default {
           orient: "vertical",
           right: "30%",
           bottom: "50%",
+          textStyle: {
+            color: "#fff"
+          }
+        },
+        //是视觉映射组件，用于进行『视觉编码』，也就是将数据映射到视觉元素（视觉通道）。
+        visualMap: {
+          min: 0, //最小值
+          max: 10, //最大值
+          left: "25%",
+          bottom: "5%",
+          calculable: true, //是否显示拖拽用的手柄（手柄能拖拽调整选中范围）。
+          inRange: {
+            color: ["#e0ffff", "#006edd"] //颜色
+          },
           textStyle: {
             color: "#fff"
           }
@@ -734,68 +679,65 @@ export default {
           }
         },
         series: [
+          // {
+          //   name: "隔离人数",
+          //   type: "scatter",
+          //   coordinateSystem: "geo",
+          //   data: convertData(this.data_gelilist),
+          //   symbolSize: function(val) {
+          //     return val[2] / 15;
+          //   },
+          //   label: {
+          //     normal: {
+          //       formatter: "{b}",
+          //       position: "right",
+          //       show: false
+          //     },
+          //     emphasis: {
+          //       show: true
+          //     }
+          //   },
+          //   itemStyle: {
+          //     normal: {
+          //       color: "#ffeb7b"
+          //     }
+          //   }
+          // },
+          // {
+          //   name: "发烧人数",
+          //   type: "effectScatter",
+          //   coordinateSystem: "geo",
+          //   data: this.data_fashaolist,
+          //   symbolSize: 10,
+          //   showEffectOn: "render",
+          //   rippleEffect: {
+          //     brushType: "stroke"
+          //   },
+          //   hoverAnimation: true,
+          //   label: {
+          //     normal: {
+          //       formatter: "{b}",
+          //       position: "right",
+          //       show: false
+          //     }
+          //   },
+          //   itemStyle: {
+          //     normal: {
+          //       color: "red",
+          //       shadowBlur: 10,
+          //       shadowColor: "rgba(0,0,0,.3)"
+          //     }
+          //   },
+          //   zlevel: 2
+          // },
           {
-            name: "隔离人数",
-            type: "scatter",
-            coordinateSystem: "geo",
-            data: convertData(data),
-            symbolSize: function(val) {
-              return val[2] / 15;
-            },
-            label: {
-              normal: {
-                formatter: "{b}",
-                position: "right",
-                show: false
-              },
-              emphasis: {
-                show: true
-              }
-            },
-            itemStyle: {
-              normal: {
-                color: "#ffeb7b"
-              }
-            }
-          },
-          {
-            name: "发烧人数",
-            type: "effectScatter",
-            coordinateSystem: "geo",
-            data: convertData(
-              data
-                .sort(function(a, b) {
-                  return b.value - a.value;
-                })
-                .slice(0, 6)
-            ),
-            symbolSize: function(val) {
-              return val[2] / 20;
-            },
-            showEffectOn: "render",
-            rippleEffect: {
-              brushType: "stroke"
-            },
-            hoverAnimation: true,
-            label: {
-              normal: {
-                formatter: "{b}",
-                position: "right",
-                show: false
-              }
-            },
-            itemStyle: {
-              normal: {
-                color: "red",
-                shadowBlur: 10,
-                shadowColor: "rgba(0,0,0,.3)"
-              }
-            },
-            zlevel: 1
-          }
+        name: "确诊病例",
+        type: "map",
+        geoIndex: 0,
+        data: this.data_alllist
+    }
         ]
       };
-
       myChart.setOption(option);
       window.addEventListener("resize", function() {
         myChart.resize();
